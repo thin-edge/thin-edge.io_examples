82119e9b01c9a7a44864e8246d947407da713b3d
Christoph Stoidner <christoph.stoidner@softwareag.com>
2022-05-23 19:58:58 +0100

    removed broken patchfile handling, improved image build

--- selfupdate-docker/docker-image/build.sh
+++ selfupdate-docker/docker-image/build.sh
@@ -1,30 +1,32 @@
 
-# prep image build
-
-mkdir -p ./tmp/
-
-# TODO: remove patch thin below
-# pickup latest change from local repo
-pushd /home/pi/workspace/selfupdate-poc/alpine/thin-edge.io_selfup >/dev/null
-git diff --staged > /tmp/repo-latest-changes.patch
-popd >/dev/null
-mv  /tmp/repo-latest-changes.patch ./tmp/
-
 # build images
 echo "**** Building tegde docker image"
 docker build -t tedge_image:v0.6.1-before-update .
-echo "**** Building a 2nd image that to be used for update"
-echo "**** NOTE: Same image as 1st but with different content in text file '/version'"
-docker build -t tedge_image:v0.6.1-after-update -f Dockerfile-new-version-dummy .
+
+
+echo "**** Copying image and changing image version text-file /image-version, to have an image to play the update process"
+docker run -it -d --name=thin-edge \
+       --mount type=bind,source=/tmp/tedge-update-store,target=/tmp/tedge-update-store \
+       --mount type=bind,source=/var/run/tedge_update,target=/var/run/tedge_update \
+       --mount type=bind,source=/usr/bin/docker,target=/usr/bin/docker \
+       --mount type=bind,source=/var/run/docker.sock,target=/run/docker.sock \
+       --mount type=bind,source=/lib/ld-linux-aarch64.so.1,target=/lib/ld-linux-aarch64.so.1 \
+       --mount type=bind,source=/lib/aarch64-linux-gnu,target=/lib/aarch64-linux-gnu \
+       tedge_image:v0.6.1-before-update
+
+docker exec thin-edge sh -c 'echo "Image from after update" >/image-version'
+docker stop thin-edge
+docker commit thin-edge tedge_image:v0.6.1-after-update
+docker rm thin-edge
 
 # save images as files
-#echo "**** Saving both images to files"
+echo "**** Saving both images to files"
+mkdir -p ./files
 docker save -o ./files/tedge_image-v0.6.1-before-update.docker-image-tarx tedge_image:v0.6.1-before-update
 docker save -o ./files/tedge_image-v0.6.1-after-update.docker-image-tarx  tedge_image:v0.6.1-after-update
 
 
 # removing images from docker
-#echo "Removing images from docker"
 docker rmi tedge_image:v0.6.1-before-update
 docker rmi tedge_image:v0.6.1-after-update
 
