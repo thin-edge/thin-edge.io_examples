a2f8be2daea424a2210c2aa85d5c3cd0205887d2
Christoph Stoidner <christoph.stoidner@softwareag.com>
2022-05-24 23:00:57 +0100

    fixed folders created in tmpfs

--- selfupdate-docker/docker-image/build.sh
+++ selfupdate-docker/docker-image/build.sh
@@ -5,6 +5,8 @@ docker build -t tedge_image:v0.6.1-before-update .
 
 
 echo "**** Copying image and changing image version text-file /image-version, to have an image to play the update process"
+sudo mkdir -p /tmp/tedge-update-store
+sudo mkdir -p /var/run/tedge_update
 docker run -it -d --name=thin-edge \
        --mount type=bind,source=/tmp/tedge-update-store,target=/tmp/tedge-update-store \
        --mount type=bind,source=/var/run/tedge_update,target=/var/run/tedge_update \
@@ -30,33 +32,3 @@ docker save -o ./files/tedge_image-v0.6.1-after-update.docker-image-tarx  tedge_
 docker rmi tedge_image:v0.6.1-before-update
 docker rmi tedge_image:v0.6.1-after-update
 
-
-echo "Manual steps left todo:
-        (*) 1) build local git: ../../thin-edge.io_selfup/ 
-	(*) 2) copy local git's plugins/tedge_docker_plugin/tedge_docker_plugin.sh into image(s)
-	(*) 3) copy local git's target/release/tedge_updater into image(s)
-
-
-        4) Assure folder do exist on host:
-	     mkdir /tmp/tedge-update-store -p
-	     mkdir /var/run/tedge_update   -p
-
-	(*) 5) save images
-	   and rmi images
-
-        6) Initially start productive container:
-              docker run -it -d --name=thin-edge \
-       --mount type=bind,source=/tmp/tedge-update-store,target=/tmp/tedge-update-store \
-       --mount type=bind,source=/var/run/tedge_update,target=/var/run/tedge_update \
-       --mount type=bind,source=/usr/bin/docker,target=/usr/bin/docker \
-       --mount type=bind,source=/var/run/docker.sock,target=/run/docker.sock \
-       --mount type=bind,source=/lib/ld-linux-aarch64.so.1,target=/lib/ld-linux-aarch64.so.1 \
-       --mount type=bind,source=/lib/aarch64-linux-gnu,target=/lib/aarch64-linux-gnu \
-       tedge_image:v0.6.1-before-update	
-
-        7) create /etc/tedge/tedge_components.toml
-           See https://github.com/cstoidner/thin-edge.io_selfup/blob/feature/759_self_update_agent/crates/core/plugin_sm/src/plugin_manager.rs#L257
-
-           Example:
-             echo -e 'components = [ \\\"tedge_1st-plugin\\\" ]' >/etc/tedge/tedge_components.toml	      
-"
