02306a7ca1dbc5c61f8c01629f8da67bf16a3103
Felix Schaede <felix.schaede@softwareag.com>
2023-01-03 17:05:47 +0100

    Added realtime in UI to deploy node-red flows

--- node-red-management-plugin/node-red-devicemanagement-plugin/src/node-red-tab.component.ts
+++ node-red-management-plugin/node-red-devicemanagement-plugin/src/node-red-tab.component.ts
@@ -1,28 +1,39 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Component, OnDestroy, OnInit, TemplateRef } from '@angular/core';
 import { OperationService } from '@c8y/client';
 import { ActivatedRoute } from '@angular/router';
 import { Flow, FlowStatus } from './shared/node-red-models';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { NodeRedFlowService } from './shared/node-red-flows.service';
+import { RealtimeHelperService } from './shared/realtimeHelper.service';
+import { Subscription } from 'rxjs';
 
 
 @Component({
   selector: 'app-node-red-tab',
   templateUrl: './node-red-tab.component.html'
 })
-export class NodeRedTabComponent implements OnInit {
+export class NodeRedTabComponent implements OnInit, OnDestroy {
 
   private deviceId: string;
   deployedFlows: Flow[] = [];
   modalRef?: BsModalRef;
   flowStatus: FlowStatus = {};
   isRealtimeActive = false;
+  operationsSubscription: Subscription;
   constructor(
-    private route: ActivatedRoute, private operations: OperationService, private modalService: BsModalService, private flows: NodeRedFlowService) { }
+    private route: ActivatedRoute, private operations: OperationService, private modalService: BsModalService, private flows: NodeRedFlowService, private realtimeService: RealtimeHelperService) { }
 
   ngOnInit(): void {
     this.deviceId = this.route.snapshot.parent.data.contextData["id"];
     this.loadData()
+    this.isRealtimeActive = true;
+    this.toggleRealtime(true);
+  }
+
+  ngOnDestroy(): void {
+    if (this.operationsSubscription) {
+      this.operationsSubscription.unsubscribe();
+    }
   }
 
   async loadData() {
@@ -51,6 +62,16 @@ export class NodeRedTabComponent implements OnInit {
       revert: true
     }
     await this.operations.list(filter)
-      .then(res => flows.forEach(f => this.flowStatus[f.id] = res.data.find(o => o.c8y_NodeRed?.flowId === f.id).status))
+      .then(res => flows.forEach(f => this.flowStatus[f.id] = res?.data.find(o => o.c8y_NodeRed?.flowId === f.id)?.status))
+  }
+
+  private toggleRealtime(on: boolean): void {
+    if (on) {
+      this.operationsSubscription = this.realtimeService.operations$(this.deviceId)
+        .subscribe(o => this.flowStatus[o.data.c8y_NodeRed?.flowId] = o.data.status)
+    }
+    else {
+      this.operationsSubscription.unsubscribe()
+    }
   }
 }
---
+++ node-red-management-plugin/node-red-devicemanagement-plugin/src/shared/realtimeHelper.service.ts
@@ -0,0 +1,36 @@
+import { RealtimeAction } from '@c8y/ngx-components';
+import { Observable } from 'rxjs';
+import { Realtime, IOperation } from '@c8y/client';
+import { Injectable } from '@angular/core';
+
+export interface RealtimeMessage<T> {
+    channel: 'operations';
+    data: T;
+    id: string;
+    realtimeAction: RealtimeAction;
+}
+
+@Injectable({ providedIn: 'root' })
+export class RealtimeHelperService {
+    constructor(private realtime: Realtime) { }
+    /**
+     * Wraps the Cumulocity operation realtime API back into an rxjs observable.
+     */
+
+    operations$(id: string): Observable<RealtimeMessage<IOperation>> {
+        const channel = 'operations';
+        const path = `/${channel}/${id}`;
+        return new Observable<any>((observer) => {
+            const subscription = this.realtime.subscribe(path, (msg) => {
+                const data: RealtimeMessage<IOperation> = {
+                    channel: msg.channel,
+                    data: msg.data.data,
+                    id: msg.id,
+                    realtimeAction: msg.data.realtimeAction,
+                };
+                observer.next(data);
+            });
+            return () => this.realtime.unsubscribe(subscription);
+        });
+    }
+}
