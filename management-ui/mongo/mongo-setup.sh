e65139ed0089ffc17d526cca480b1cbd6a29110b
sagIoTPower <christof.strack@softwareag.com>
2023-01-03 16:32:54 +0100

    up. management-ui to use tedge 0.8.1 and mongo 6.x

--- management-ui/mongo/mongo-setup.sh
+++ management-ui/mongo/mongo-setup.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /data/mongo-init.flag ]; then
     echo "Init replicaset"
-    mongo mongodb://mongodb1:27017/localDB mongo-setup.js
+    mongosh mongodb://mongodb1:27017/localDB mongo-setup.js
     touch /data/mongo-init.flag
 else
     echo "Replicaset already initialized"
