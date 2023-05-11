6c315323d0c14c228f060090b36c06cc71c34638
Felix_Schaede <felix.schaede@softwareag.com>
2023-05-11 08:01:04 +0200

    Node-red webapp to 10.16

--- node-red-management-plugin/node-red-devicemanagement-plugin/src/node-red-device.module.ts
+++ node-red-management-plugin/node-red-devicemanagement-plugin/src/node-red-device.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CoreModule, HOOK_ONCE_ROUTE, ViewContext, FormsModule } from '@c8y/ngx-components';
+import { CoreModule, ViewContext, FormsModule, HOOK_ROUTE } from '@c8y/ngx-components';
 import { AddFlowModal } from './add-flow-modal/add-flow-modal.component';
 import { ItemSelector } from './add-flow-modal/item_selector/item-selector.component';
 import { NodeRedDeviceGuard } from './node-red-device.guard';
@@ -27,7 +27,7 @@ const routes: Routes = [
         NodeRedFlowService,
         RealtimeHelperService,
         {
-            provide: HOOK_ONCE_ROUTE,
+            provide: HOOK_ROUTE,
             multi: true,
             useValue: [{
                 context: ViewContext.Device,
